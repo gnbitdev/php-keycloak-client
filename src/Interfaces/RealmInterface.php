429e313bf9983f2c175f01ff26bde03bdaa6e4c6
benjamin <benjamin@gnbit.com>
2021-12-01 12:19:29 -0500

    Add licence in composer

--- src/Interfaces/RealmInterface.php
+++ src/Interfaces/RealmInterface.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace KeycloakApiClient\Interfaces;
+
 interface RealmInterface
 {
+
     public function getName();
+
     public function setName();
+
     public function getDescription();
+
     public function setDescription();
 }

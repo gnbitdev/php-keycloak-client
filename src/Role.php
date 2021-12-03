8f924eb98959e2c5d523d4d11c3def697ae61d49
Joan Perez Alvarado <41269373+joanjpx@users.noreply.github.com>
2021-12-03 13:25:09 -0500

    + added property $attributes @ Role Class

--- src/Role.php
+++ src/Role.php
@@ -7,13 +7,21 @@ class Role
     public $id;
     private $name;
     private $description;
+    private $attributes;
+
 
     ## constructor
-    public function __construct($id='', $name='', $description='')
+    public function __construct(
+        String $id='', 
+        String $name='', 
+        String $description='', 
+        Array $attributes=[]
+    )
     {
         $this->id = $id;
         $this->name = $name;
         $this->description = $description;
+        $this->attributes = $attributes;
     }
 
     ## getters and setters
@@ -47,12 +55,25 @@ class Role
         $this->description = $description;
     }
 
+    public function getAttributes() : Array
+    {
+        return $this->attributes;
+    }
+
+    public function setAttributes(Array $attributes)
+    {
+        $this->attributes = $attributes;
+    }
+
+
+
     public function toArray() : Array
     {
         return array(
             'id' => $this->id,
             'name' => $this->name,
-            'description' => $this->description
+            'description' => $this->description,
+            'attributes' => $this->attributes
         );
     }
 

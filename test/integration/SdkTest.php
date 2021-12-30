2f2b094f488bb0c8734ef15683c69bef67604adf
benjamin <benjamin@gnbit.com>
2021-12-30 17:42:46 -0500

    redoing work

---
+++ test/integration/SdkTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace KeycloakApiCLientIntegrationTest;
+
+use PHPUnit\Framework\TestCase;
+
+class Apitest extends TestCase
+{
+
+    protected function setUp()
+    {
+
+    }
+
+    public function testConnect()
+    {
+
+
+        self::assertInternalType('int', 1);
+    }
+}
